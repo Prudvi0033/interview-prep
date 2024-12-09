f56ac4ef07e0ef506a941f2d6613ccd6929fcdd0
Prudvi <prudvirajvarma2004A=@gmail.com>
2024-12-09 13:13:11 +0530

    added descrption

--- client/src/components/Topics.jsx
+++ client/src/components/Topics.jsx
@@ -13,12 +13,12 @@ const Topics = () => {
     <div>
       <Navbar />
       <div className="pt-20 gap-4 grid grid-cols-3">
-        <Items icon={C} title={"C"} desc={"Strengthen your understanding of procedural programming, memory management, and system-level programming with C."} />
-        <Items icon={py} title={"C"} desc={"Strengthen your understanding of procedural programming, memory management, and system-level programming with C."} />
-        <Items icon={java} title={"C"} desc={"Strengthen your understanding of procedural programming, memory management, and system-level programming with C."} />
-        <Items icon={cpp} title={"C"} desc={"Strengthen your understanding of procedural programming, memory management, and system-level programming with C."} />
-        <Items icon={sql} title={"C"} desc={"Strengthen your understanding of procedural programming, memory management, and system-level programming with C."} />
-        <Items icon={dsa} title={"C"} desc={"Strengthen your understanding of procedural programming, memory management, and system-level programming with C."} />
+      <Items icon={C} title={"C"} desc={"Strengthen your understanding of procedural programming, memory management, and system-level programming with C."} />
+        <Items icon={py} title={"Python"} desc={"Master versatile programming concepts, scripting, and problem-solving with Python's simple syntax and powerful libraries."} />
+        <Items icon={java} title={"Java"} desc={"Develop expertise in object-oriented programming, multi-threading, and core Java concepts for robust application development."} />
+        <Items icon={cpp} title={"C++"} desc={"Strengthen knowledge in object-oriented programming, STL, memory management, and performance-driven application design."} />
+        <Items icon={sql} title={"MySQL"} desc={"Master querying, manipulating, and managing data in relational databases using SQL commands like SELECT, JOIN, and GROUP BY."} />
+        <Items icon={dsa} title={"Data Structures"} desc={"Learn to design, implement, and optimize data structures like arrays, linked lists, stacks, queues, trees, and graphs for efficient problem-solving."} />
       </div>
     </div>
   );
